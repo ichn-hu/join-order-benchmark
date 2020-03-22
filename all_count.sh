bb5d894e98b7153cfaffad839cd4a12664e84cae
Zhifeng Hu <huzhfeng@pingcap.com>
2020-03-22 11:09:58 +0800

    add count

--- all_count.sh
+++ all_count.sh
@@ -3,9 +3,12 @@ CURDIR=$(cd `dirname $0`; pwd)
 cd $CURDIR
 
 ls $CURDIR/csv_files/*.csv | while read file; do
+  cnt=$(cat $file | wc -l)
   table=`basename $file`
   table=${table%.*}
-  #echo "select count(*) $table from $table;"
-  #echo "ANALYZE TABLE $table;"
-  echo $table
+  in_db_cnt=$(mysql -h 127.0.0.1 -P 4000 -u root -e "use imdbload; select count(*) from $table;" | sed -sn 2p)
+  if [ $cnt -ne $in_db_cnt ]
+  then
+    echo $table $cnt $in_db_cnt $(($cnt-$in_db_cnt))
+  fi
 done
